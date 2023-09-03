ba90052ed6a02d3e2d2aab9f3f2d73f05110b2ba
Diddi <dps.dtt25@gmail.com>
2023-09-03 21:16:12 +0200

    genererade arrays

---
+++ src/GenerateArrays.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class GenerateArrays {
+
+    public static int[] generateUnsorted(int length) {
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] = new Random().nextInt();
+        }
+        return array;
+    }
+
+    public static int[] generateSorted(int length) {
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            array[i] += new Random().nextInt() + 1;
+        }
+        return array;
+    }
+}
