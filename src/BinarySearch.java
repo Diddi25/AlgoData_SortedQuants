43fede6189f44cdcff440536e1155d1d2162ae3e
Diddi <dps.dtt25@gmail.com>
2023-09-03 22:32:03 +0200

    påväg

---
+++ src/BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+
+    public static boolean binarySearch(int[] array, int key) {
+        int min = 0;
+        int max = array.length - 1;
+
+        while(min != max) {
+
+        }
+        return false;
+    }
+}
