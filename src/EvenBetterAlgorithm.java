a493f7ee120269ef61e87f08c2ab989c9f194bea
Diddi <dps.dtt25@gmail.com>
2023-09-04 22:53:48 +0200

    copied even-better-algorithm to underConstruct

--- src/EvenBetterAlgorithm.java
+++ src/EvenBetterAlgorithm.java
@@ -1,3 +1,25 @@
 public class EvenBetterAlgorithm {
 
+    public static boolean findDuplicatesInTwo(int[] array1, int[] array2) {
+        int min_array1 = 0;
+        int min_array2 = 0;
+        int max_array1 = array1.length - 1;
+        int max_array2 = array2.length - 1;
+
+        while(min_array1 <= max_array1 && min_array2 <= max_array2) {
+            int mid_array1 = (min_array1 + max_array1) / 2;
+            int mid_array2 = (min_array2 + max_array2) / 2;
+
+            if (array2[mid_array2] == array1[mid_array1]) {
+                return true;
+            } else if (array2[mid_array2] < array1[mid_array1]) {
+                min_array2 = mid_array2 + 1;
+            } else {
+                min_array1 = mid_array1 + 1;
+            }
+        }
+
+        return false;
+    }
+
 }
