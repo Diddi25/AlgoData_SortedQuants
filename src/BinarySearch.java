c2e707f6274d66e6517ccc6757cf177e17316b07
Diddi <dps.dtt25@gmail.com>
2023-09-04 14:15:54 +0200

    förhoppningsvis fungerande binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,12 +1,22 @@
 public class BinarySearch {
 
-    public static boolean binarySearch(int[] array, int key) {
+    public static boolean binarySearch(int[] array, int value) {
         int min = 0;
         int max = array.length - 1;
 
-        while(min != max) {
+        while (min <= max) {
+            int middle = (min + max) / 2;
 
+            if(array[middle] == value) {
+                return true;
+            }
+            if(value < array[middle]) {
+                max = middle - 1;
+            } else {
+                min = middle + 1;
+            }
         }
+
         return false;
     }
 }
