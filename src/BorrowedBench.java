d63508bf59ac958341378cbd7fbadc72ae518dc4
Diddi <diddi.dutta@outlook.com>
2023-09-05 11:33:26 +0200

    måste få even-better att funka

--- src/BorrowedBench.java
+++ src/BorrowedBench.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 
-
 class BorrowedBench {
 
     private static void linear(int[] array, int[] index) {
@@ -9,13 +8,15 @@ class BorrowedBench {
         }
     }
 
-
     private static void binary(int[] array, int[] index) {
         for (int i = 0; i < index.length ; i++) {
             BinarySearch.search(array, index[i]);
         }
     }
 
+    private static void evenBetter(int[] array1, int[] array2) {
+        EvenEvenBetterAlgorithm.linearDuplicatesSearch(array1, array2);
+    }
 
     public static int[] sorted(int n) {
         Random random = new Random();
@@ -29,59 +30,73 @@ class BorrowedBench {
         return array;
     }
 
-
     public static int[] keys(int loop, int n) {
         Random random = new Random();
-        int[] index = new int[loop];
+        int[] keys = new int[loop];
         for (int i = 0; i < loop ; i++) {
-            index[i] = random.nextInt(n*5);
+            keys[i] = random.nextInt(n*5);
         }
-        return index;
+        return keys;
     }
-
 
     public static void main(String[] arg) {
 
-        int[] sizes = {100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600};
+        int[] sizes = {100, 200, 400, 800, 1600, 3200, 6400};
 
         System.out.printf("# searching through an array of length n, time in ns\n");
-        System.out.printf("#%7s%8s%8s\n", "n", "linear", "binary");
+        System.out.printf("#%7s%8s%8s%14s\n", "n", "linear", "binary", "even-better");
         for ( int n : sizes) {
 
             int loop = 10000;
 
-            int[] array = sorted(n);
+            int[] array1 = sorted(n);
+            int[] array2 = sorted(n);
             int[] index = keys(loop, n);
 
             System.out.printf("%8d", n);
 
-            int k = 1000;
+            float k = 1000;
 
-            double min = Double.POSITIVE_INFINITY;
+            float min = Float.POSITIVE_INFINITY;
 
             for (int i = 0; i < k; i++) {
                 long t0 = System.nanoTime();
-                linear(array, index);
+                linear(array1, index);
                 long t1 = System.nanoTime();
-                double t = (t1 - t0);
+                float t = (t1 - t0);
                 if (t < min)
                     min = t;
             }
 
             System.out.printf("%8.0f", (min/loop));
 
-            min = Double.POSITIVE_INFINITY;
+            min = Float.POSITIVE_INFINITY;
 
             for (int i = 0; i < k; i++) {
                 long t0 = System.nanoTime();
-                binary(array, index);
+                binary(array1, index);
                 long t1 = System.nanoTime();
-                double t = (t1 - t0);
+                float t = (t1 - t0);
                 if (t < min)
                     min = t;
             }
 
-            System.out.printf("%8.0f\n", (min/loop));
+            System.out.printf("%8.0f", (min/loop));
+
+            min = Float.POSITIVE_INFINITY;
+
+            for (int i = 0; i < 5; i++) {
+                long t0 = System.nanoTime();
+                evenBetter(array1, array2);
+                long t1 = System.nanoTime();
+                float t = (t1 - t0);
+                if (t < min)
+                    min = t;
+            }
+
+            float result = (min/loop);
+
+            System.out.printf("%8.0f\n", result);
         }
     }
 }
