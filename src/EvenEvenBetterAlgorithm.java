19309e814324b2fe798d6829cc200b38cf214d02
Diddi <diddi.dutta@outlook.com>
2023-09-05 10:51:17 +0200

    The even even better algorithm implemented

---
+++ src/EvenEvenBetterAlgorithm.java
@@ -0,0 +1,18 @@
+public class EvenEvenBetterAlgorithm {
+    public static boolean linearDuplicatesSearch(int[] array1, int[] array2) {
+        int pointer_1 = 0;
+        int pointer_2 = 0;
+
+        while(pointer_1 == array1.length && pointer_2 == array2.length) {
+            if(array1[pointer_1] == array2[pointer_2]) {
+                return true;
+            } else if (array1[pointer_1] < array2[pointer_2]){
+                pointer_1++;
+            } else {
+                pointer_2++;
+            }
+        }
+
+        return false;
+    }
+}
